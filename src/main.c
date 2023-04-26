c36f0d964b7efd3cbe57dccc57ffc3665b3d6764
Electro-Corp <chinmaytanmay@outlook.com>
2023-04-25 21:53:56 -0600

    bruh

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ typedef struct {
 	int r,g,b;
 } color;
 void usage();
-unsigned concatenate(unsigned x, unsigned y);
 int main(int args, char* argv[]){
   int height = 0;
   int width = 0;
@@ -20,70 +19,49 @@ int main(int args, char* argv[]){
 	fread(buffer,sizeof(buffer),1,fp);
 	// load colors
 	color Colors[(sizeof(buffer)/sizeof(buffer[0]) - 6) / 6];
-	int g = 0, no = 0, i = 13;
+	int g = 0;
 	// get the width / height
-	while(no < 3)
-		if(buffer[i++] == '\n')
-		no++; 
-  char* m = (char*)malloc((sizeof(char) *  (sizeof(buffer)/sizeof(buffer[0]))));
-  strcpy(m,buffer);
-  char* p = strtok(m," ");
-  char* j = strtok(p, "\n");
-  j = strtok(NULL, "\n");
-  width = atoi(j);
-  char* out;
-  out = strtok(buffer," ");
-  //printf("init = %s\n",out);
-  int count = 0;
-  color tmp = {0,0,0};
-  int k = -1;
-  while (out != NULL)
-  {  
-    if(count < 3){
-        int c = 0;
-        sscanf(out, "%d", &c);
-        switch(count){
-          case 0:
-          tmp.r = c;
-          break;
-          case 1:
-          tmp.g = c;
-          break;
-          case 2:
-          tmp.b = c;
-          break;
-          default:
-          printf("guh?\n");
-          exit(1);
-          break;
-        }
-    }else{
-      count = 0;
-      Colors[g++] = tmp;
-    }
-    count++;
-    
-    out = strtok(NULL, " ");
-  }
-  int c = 0;
-	for(int i = 0; i < g; i++){
-    //printf("%d,%d,%d \n",Colors[i].r,Colors[i].g, Colors[i].b);
-    //getchar();
-    printf("\033[48;2;%d;%d;%dm ",Colors[c].r,Colors[c].g, Colors[c].b);
-    if(i % 1 == 0)c++;
-    if(i % (width) == 0)printf("\n");
-  }
+	char* m = (char*)malloc((sizeof(char) *  (sizeof(buffer)/sizeof(buffer[0]))));
+	strcpy(m,buffer);
+	char* p = strtok(m," ");
+	char* j = strtok(p, "\n");
+	j = strtok(NULL, "\n");
+	width = atoi(j);
+	// 
+	char* out = strtok(buffer," \n");
+	color tmp = {0,0,0};
+	int k = 0, c = 0, count = 0;
+	while (out != NULL){  		
+		//printf("=============[NEW]================\n");
+		//printf("[CURRENT] = %s\n ==== \n",out);
+		//getchar();
+		if(k == 2) height = atoi(out);
+		if(count <= 2 && k > 2){
+			//printf("[USING] = %s\n ===== \n",out);
+			sscanf(out, "%d", &c);
+			switch(count){
+				case 0: tmp.r = c; break;
+				case 1: tmp.g = c; break;
+				case 2: tmp.b = c; break;
+			}
+			count++;
+		}else if(k > 2){
+			count = 0;
+			Colors[g++] = tmp;
+			printf("[GOT DAT] %d;%d;%d\n",Colors[g-1].r,Colors[g-1].g, Colors[g-1].b);
+			getchar();
+		}
+		k++;
+		out = strtok(NULL, " \n");
+	}
+	for(int i = 0, c = 0; i < g*2; i++){
+		printf("\033[48;2;%d;%d;%dm ",Colors[c].r,Colors[c].g, Colors[c].b);
+		if(i % 2 == 0)c++;
+		if(i % (width*2) == 0)printf("\n");
+	}
+	printf("\nInfo, %d pixels found, expected %d, diff of %d\n",g,width*height, width*height - g);
 }
 void usage(){
 	printf("./tppm [filename]\n");
 	exit(0);
 }
-
-
-unsigned concatenate(unsigned x, unsigned y) {
-    unsigned pow = 10;
-    while(y >= pow)
-        pow *= 10;
-    return x * pow + y;        
-}
-
